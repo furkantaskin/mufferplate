580ad67cec212e43429e166c199001476514406f
Furkan Taşkın <furkan.taskin@tuta.io>
2022-12-07 11:16:01 +0300

    Initial commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+
+</footer>
+<script src="assets/plugins/swiper/swiper-bundle.min.js"></script>
+<script src="assets/plugins/lightgallery/src/lightgallery.min.js"></script>
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include "header.php"; ?>
+<main>
+    
+</main>
+<?php include "footer.php"; ?>
