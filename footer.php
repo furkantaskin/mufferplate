2636fc76b097fb1c4001850614c45b4cb2b7fc3c
Furkan Taşkın <furkan.taskin@tuta.io>
2022-12-23 16:08:28 +0300

    Make js file dynamic for each page

--- footer.php
+++ footer.php
@@ -1,10 +1,6 @@
 <footer>
 
 </footer>
-<script src="assets/plugins/swiper/swiper-bundle.min.js"></script>
-<script src="assets/plugins/lightgallery/src/lightgallery.min.js"></script>
-<script src="assets/js/jquery.js"></script>
-<script src="assets/plugins/rellax/rellax.min.js"></script>
-<script src="assets/js/main.js"></script>
+<script src="assets/js/<?=$callFile?>.js"></script>
 </body>
 </html>
