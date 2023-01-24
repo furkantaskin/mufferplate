6cf5096355585d009b80ce62ca4677549a3afdf9
Furkan Taşkın <furkan.taskin@tuta.io>
2023-01-24 12:29:50 +0300

    BREAKING CHANGE: close #5 -> Add esbuild as bundler

---
+++ esbuild.config.mjs
@@ -0,0 +1,28 @@
+import * as esbuild from 'esbuild';
+import { URL } from 'url';
+
+const startTime = new Date().getTime();
+const outdir = new URL(`./theme/assets/js/`, import.meta.url)
+  .pathname;
+
+function mergeFiles(filePath) {
+  return new URL(`./src/pages/${filePath}`, import.meta.url).pathname;
+}
+
+await esbuild
+  .build({
+    entryPoints: [mergeFiles('home.js')],
+    bundle: true,
+    minify: true,
+    treeShaking: true,
+    sourcemap: 'external',
+    outdir: outdir,
+  })
+  .then(() =>
+    console.log(
+      `Bundling is done in ${
+        (new Date().getTime() - startTime) / 1000
+      } s and outdir is ${outdir}`
+    )
+  )
+  .catch((error) => console.log(`Error: ${error}`));
