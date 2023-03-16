04b848852f87e2e00a9557e6c89733319412a825
Furkan Taşkın <furkan.taskin@tuta.io>
2023-03-16 09:37:44 +0300

    Format the code

--- theme/index.php
+++ theme/index.php
@@ -4,8 +4,8 @@ $call_js = "home";
 include "header.php"; 
 ?>
 <main>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 53.7 53.7">
-                                    <use xlink:href="<?= domain ?>assets/img/svg/svgtemplate.svg#youtube"/>
-                                </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 53.7 53.7">
+        <use xlink:href="<?= domain ?>assets/img/svg/svgtemplate.svg#youtube"/>
+    </svg>
 </main>
 <?php include "footer.php"; ?>
