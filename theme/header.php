fa39c663d9b9b3b85a3e75884745542d678ed03c
Furkan Taşkın <furkan.taskin@tuta.io>
2023-03-16 09:55:58 +0300

    Remove unused bootstrap file call

--- theme/header.php
+++ theme/header.php
@@ -13,7 +13,6 @@ define("domain", "http://$_SERVER[HTTP_HOST]/boilerplate/theme/");
     <title>Starter Kit</title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="/src/css/vendors/bootstrap.min.css">
     <link rel="stylesheet" href="<?=domain?>assets/css/<?=$call_file?>.css?v=<?=rand()?>">
 </head>
 <body>
