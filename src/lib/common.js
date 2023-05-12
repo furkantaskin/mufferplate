1e2837762b2698a3a98020b71868246065b01d75
Furkan Taşkın <furkan.taskin@tuta.io>
2023-05-12 18:19:12 +0300

    Remove function for title set

--- src/lib/common.js
+++ src/lib/common.js
@@ -30,12 +30,3 @@ export function mobileMenu() {
   mobileMenu.style.paddingTop =
     document.querySelector('.mobileHeader').clientHeight * 1.5 + 'px';
 }
-
-
-export function setTitle(){
-  document.querySelectorAll("a").forEach((e) => {
-    if (e.innerText){
-      e.setAttribute("title", e.innerText);
-    }
-  });
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 import { Collapse } from 'bootstrap';
 import PhotoSwipeLightbox from 'photoswipe/lightbox';
-import { mobileMenu, setTitle } from '../lib/common';
+import { mobileMenu } from '../lib/common';
 
 mobileMenu();
-setTitle();
 
 document.querySelectorAll('.prodgallery a img').forEach((e) => {
   e.parentElement.dataset.pswpWidth = e.naturalWidth;
