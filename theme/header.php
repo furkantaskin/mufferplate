49b4870c9f8aaa5c4806e84d174fb354b49c169e
Furkan Taşkın <furkan.taskin@tuta.io>
2023-06-16 19:23:01 +0300

    Add class name option for svg function

--- theme/header.php
+++ theme/header.php
@@ -4,14 +4,14 @@ $directory = explode("/", $_SERVER['REQUEST_URI'])[1];
 define("domain", "http://$_SERVER[HTTP_HOST]/$directory/theme/");
 
 
-function getSprite($spriteId)
+function getSprite($spriteId, $className = "")
 {
     $spriteSheetPath = domain . 'assets/img/svg/sprite.svg';
     $spriteSheet = new SimpleXMLElement(file_get_contents($spriteSheetPath));
     $sprite = $spriteSheet->xpath("//*[@id='$spriteId']")[0];
     $viewBox = (string)$sprite['viewBox'];
     return
-        "<svg xmlns='http://www.w3.org/2000/svg' viewBox='$viewBox'>
+        "<svg xmlns='http://www.w3.org/2000/svg' viewBox='$viewBox' class='$className'>
 <use xlink:href='" . domain . "assets/img/svg/sprite.svg#" . $spriteId . "'/>
 </svg>";
 }
