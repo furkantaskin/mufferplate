b85993476c3e89362ba0efdbc06fdbf8b1cec927
Furkan Taşkın <furkan.taskin@tuta.io>
2023-06-16 18:30:16 +0300

    Create responsive mixin for blaze tracker to prevent content layout shift

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
+import BlazeSlider from "blaze-slider";
 import { Collapse } from 'bootstrap';
 import PhotoSwipeLightbox from 'photoswipe/lightbox';
 import { mobileMenu } from '../lib/common';
 
 mobileMenu();
+
+const exampleBlaze = document.querySelector(".example-blaze");
+
+new BlazeSlider(exampleBlaze, {
+  all: {
+    slideGap: "0px",
+    slidesToShow: 1
+  }
+});
 
 document.querySelectorAll('.prodgallery a img').forEach((e) => {
   e.parentElement.dataset.pswpWidth = e.naturalWidth;
