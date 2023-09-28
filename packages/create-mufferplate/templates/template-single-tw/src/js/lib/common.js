536a6c69b03b7813ae502679d8906d1960eed7dc
Furkan Taşkın <furkan.taskin@tuta.io>
2023-09-28 19:07:54 +0300

    Add handmade library for grid layout, container and utilities

--- packages/create-mufferplate/templates/template-single-tw/src/js/lib/common.js
+++ packages/create-mufferplate/templates/template-single-tw/src/js/lib/common.js
@@ -1,6 +1,8 @@
 export function mobileMenu() {
   let showMenu = false;
   const mobileMenu = document.querySelector('.mobileMenu');
+  const mainNav = document.querySelector('.header_lg nav')
+  const mobileNav = mobileMenu.querySelector('.mobileMenu nav')
   function toggleMenu(isActive) {
     if (isActive) {
       document
@@ -24,9 +26,8 @@ export function mobileMenu() {
       toggleMenu(showMenu);
     });
 
-  mobileMenu.innerHTML =
-    document.querySelector('nav.navbar').innerHTML;
+  mobileNav.innerHTML = mainNav.innerHTML;
 
   mobileMenu.style.paddingTop =
-    document.querySelector('.mobileHeader').clientHeight * 1.5 + 'px';
+    document.querySelector('.header_sm').clientHeight * 1.5 + 'px';
 }
