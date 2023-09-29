ffc33a8a29efbaff1ca1bfcf01f741424f41ff1b
Furkan Taşkın <furkan.taskin@tuta.io>
2023-09-29 17:59:56 +0300

    Enable fetch in mobile menu

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,46 +1,6 @@
 import Swiper from 'swiper';
 import {Navigation, Pagination} from 'swiper/modules';
 import BlazeSlider from "blaze-slider";
+import { mobileMenu } from '../lib/common'; mobileMenu();
 import { Collapse } from 'bootstrap';
 import PhotoSwipeLightbox from 'photoswipe/lightbox';
-import { mobileMenu, sendUrl } from '../lib/common';
-
-mobileMenu();
-sendUrl();
-
-const exampleBlaze = document.querySelector(".example-blaze");
-
-new BlazeSlider(exampleBlaze, {
-  all: {
-    slideGap: "0px",
-    slidesToShow: 1
-  }
-});
-
-document.querySelectorAll('.prodgallery a img').forEach((e) => {
-  e.parentElement.dataset.pswpWidth = e.naturalWidth;
-  e.parentElement.dataset.pswpHeight = e.naturalHeight;
-});
-
-const accordionParent = document.getElementById('abilitiesFaq');
-
-new Collapse(accordionParent, {
-  toggle: false,
-});
-
-new Swiper('.swiper', {
-  modules: [Navigation, Pagination],
-});
-
-document.querySelectorAll('.prodgallery a img').forEach((e) => {
-  e.parentElement.dataset.pswpWidth = e.naturalWidth;
-  e.parentElement.dataset.pswpHeight = e.naturalHeight;
-});
-
-const prodGallery = new PhotoSwipeLightbox({
-  gallery: '.prodgallery',
-  children: 'a',
-  pswpModule: () => import('photoswipe'),
-});
-
-prodGallery.init();
