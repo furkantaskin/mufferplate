423d6203cfbd47858154335269030118489ba45e
Furkan Taşkın <furkan.taskin@tuta.io>
2023-10-10 16:32:02 +0300

    Remove redundante files from main directory

--- src/js/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Swiper from 'swiper';
-import {Navigation, Pagination} from 'swiper/modules';
-import BlazeSlider from "blaze-slider";
-import { mobileMenu } from '../lib/common'; mobileMenu();
-import { Collapse } from 'bootstrap';
-import PhotoSwipeLightbox from 'photoswipe/lightbox';
--- theme/components/define-domain.php
+++
@@ -1,4 +0,0 @@
-<?php
-$directory = explode("/", $_SERVER['REQUEST_URI'])[1];
-define("domain", "http://$_SERVER[HTTP_HOST]/$directory/theme/");
-?>
