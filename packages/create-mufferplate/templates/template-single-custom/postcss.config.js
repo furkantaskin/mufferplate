7e9490c119b4b477b5fc31f3332be7883012071d
Furkan Taşkın <furkan.taskin@tuta.io>
2023-10-25 13:49:37 +0300

    Add plugin to sort media queries

--- packages/create-mufferplate/templates/template-single-custom/postcss.config.js
+++ packages/create-mufferplate/templates/template-single-custom/postcss.config.js
@@ -1,16 +1,20 @@
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import purgecss from '@fullhuman/postcss-purgecss';
+import sortMediaQueries from "postcss-sort-media-queries";
 
 const postcssConfig = {
   plugins: [
     autoprefixer,
+    sortMediaQueries({
+      sort: 'desktop-first'
+    }),
     cssnano({
       preset: 'default',
     }),
     purgecss({
       content: ['./theme/**/*.php', './theme/**/*.js'],
-      safelist: [],
+      safelist: ['swiper-pagination-bullet'],
       defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
     }),
   ],
