5387cd2443bcd3662c097da4d86082ce2dd28787
Furkan Taşkın <furkan.taskin@tuta.io>
2023-12-20 20:35:17 +0300

    Add inline css option

--- packages/create-mufferplate/templates/template-single-custom/theme/header.php
+++ packages/create-mufferplate/templates/template-single-custom/theme/header.php
@@ -2,7 +2,7 @@
 
 include "components/define-domain.php";
 include "components/svg-sprite.php";
-
+$inline_css = false;
 ?>
 <!doctype html>
 <html lang="tr" dir="ltr">
@@ -21,7 +21,17 @@ include "components/svg-sprite.php";
     <link media="print" onload="this.onload=null;this.removeAttribute('media');"
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,700&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="<?= domain ?>assets/css/main.css<?=$_SERVER['REMOTE_ADDR'] == '::1' ? '?v='.rand() : ''?>">
+    <?php if ($inline_css == true): ?>
+        <style>
+            <?php
+            $css_file = file_get_contents(domain . 'assets/css/main.css');
+            echo str_replace('../', domain . 'assets/', $css_file);
+            ?>
+        </style>
+    <?php else: ?>
+        <link rel="stylesheet"
+            href="<?= domain ?>assets/css/main.css<?= $_SERVER['REMOTE_ADDR'] == '::1' ? '?v=' . rand() : '' ?>">
+    <?php endif; ?>
 </head>
 
 <body>
