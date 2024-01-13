9d1a579f0f8f04176f087f074a7a6749dab9567c
Furkan Taşkın <furkan.taskin@tuta.io>
2024-01-13 17:06:10 +0300

    Add inline css option, remove Google fonts, remove maximum-scale

--- packages/create-mufferplate/templates/template-single-tw-scss/theme/header.php
+++ packages/create-mufferplate/templates/template-single-tw-scss/theme/header.php
@@ -2,26 +2,30 @@
 
 include "components/define-domain.php";
 include "components/svg-sprite.php";
-
+$inline_css = false;
 ?>
 <!doctype html>
 <html lang="tr" dir="ltr">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="Starter kit for people">
     <title>Starter Kit</title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link media="print" onload="this.onload=null;this.removeAttribute('media');"
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,700&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="<?= domain ?>assets/css/app.css<?=$_SERVER['REMOTE_ADDR'] == '::1' ? '?v='.rand() : ''?>">
+    <?php if ($inline_css == true): ?>
+        <style>
+            <?php
+            $css_file = file_get_contents(domain . 'assets/css/app.css');
+            echo str_replace('../', domain . 'assets/', $css_file);
+            ?>
+        </style>
+    <?php else: ?>
+        <link rel="stylesheet"
+            href="<?= domain ?>assets/css/app.css<?= $_SERVER['REMOTE_ADDR'] == '::1' ? '?v=' . rand() : '' ?>">
+    <?php endif; ?>
 </head>
 
 <body>
