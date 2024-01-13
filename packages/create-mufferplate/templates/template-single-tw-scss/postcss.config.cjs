855644acf86836456da4f250001d011b8a296aa0
Furkan Taşkın <furkan.taskin@tuta.io>
2024-01-13 15:12:59 +0300

    Add missing rem-to-px converter

--- packages/create-mufferplate/templates/template-single-tw-scss/postcss.config.cjs
+++ packages/create-mufferplate/templates/template-single-tw-scss/postcss.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
+    '@thedutchcoder/postcss-rem-to-px': {},
     tailwindcss: {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
