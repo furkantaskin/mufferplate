787920cbd053a25240d1a3d4fa0146309d72829d
Furkan Taşkın <furkan.taskin@tuta.io>
2024-07-09 20:50:06 +0300

    Add publish script

--- sandbox/esbuild.js
+++ sandbox/esbuild.js
@@ -1,10 +1,25 @@
 import esbuild from "esbuild";
 import process from "node:process";
-
+import fs from "node:fs";
+import path from "node:path";
 
 const isDev = process.argv[2] === "dev";
+const srcPath = path.join(process.cwd(), "/dist/mufferplate.js");
+const targetPath = path.join(
+  process.cwd(),
+  "../packages/mufferplate/bin/mufferplate.js"
+);
 
-esbuild
+if (process.argv[2] === "publish") {
+  fs.copyFile(srcPath, targetPath, (err) => {
+    if (err) {
+      console.log("Error occurred:", err);
+    } else {
+      console.log("Dist file updated");
+    }
+  });
+} else {
+  esbuild
     .build({
       entryPoints: ["./m_src/main_mufferplate.ts"],
       outfile: "./dist/mufferplate.js",
@@ -18,6 +33,7 @@ esbuild
       platform: "node",
       packages: "external",
       tsconfig: "./tsconfig.json",
-      charset: 'utf8',
+      charset: "utf8",
     })
     .catch(() => process.exit(1));
+}
